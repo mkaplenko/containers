378689de6c060336a929c05440d3c90edffd5c8b
mkaplenko <mvkaplenko@gmail.com>
2015-06-29 00:08:58 +0300

    Add UserMixin for User model class

--- src/auth/models.py
+++ src/auth/models.py
@@ -3,11 +3,15 @@ __author__ = 'mkaplenko'
 
 from containers import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import current_app
+from flask.ext.login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
@@ -24,6 +28,22 @@ class User(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    @classmethod
+    def create(cls, email, password, is_admin=False, first_name=None, last_name=None):
+        user = cls()
+        user.email = email
+        user.first_name = first_name
+        user.last_name = last_name
+        user.is_admin = is_admin
+        user.password = password
+
+        db.session.add(user)
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            current_app.logger.exception(e)
 
 
 @login_manager.user_loader
